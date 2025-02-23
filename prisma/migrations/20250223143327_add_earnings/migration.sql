2b776601f665bceca0e2cc7d220048e02cbe0377
ucchy108 <ucchy108@gmail.com>
2025-02-24 00:27:10 +0900

    feat: earningモデルを追加した

---
+++ prisma/migrations/20250223143327_add_earnings/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `users` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
+
+-- CreateTable
+CREATE TABLE `earnings` (
+    `id` VARCHAR(36) NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `amount` INTEGER NOT NULL,
+    `date` DATETIME(3) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
