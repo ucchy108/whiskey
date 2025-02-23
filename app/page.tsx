5484367e18ddb67001981d4e26343cb0e5c83325
ucchy108 <ucchy108@gmail.com>
2025-02-24 00:28:11 +0900

    feat: ORMが取得できるかを確認するコードを追加した

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import { prisma } from "@/lib/prisma/prisma";
 import { Typography } from "@mui/material";
+import { Earning } from "@prisma/client";
 
-export default function Home() {
+export default async function Home() {
+  const earnings = await prisma.earning.findMany();
+
   return (
     <div>
       <Typography variant="h1">Test</Typography>
+      {earnings.map((earning: Earning) => (
+        <Typography key={earning.id}>{earning.amount}</Typography>
+      ))}
     </div>
   );
 }
