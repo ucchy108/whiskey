3ea678d21cf3f803ce3390e10896ca9e0fbe1c75
ucchy108 <ucchy108@gmail.com>
2025-04-05 01:22:45 +0900

    chore: サインインページにフォームを追加した

--- my-app/src/app/(auth)/signin/page.tsx
+++ my-app/src/app/(auth)/signin/page.tsx
@@ -5,17 +5,41 @@ import {
   SignInPasswordTextField,
   SignInSubmitButton,
 } from "@/features/signin";
+import { SignInFormSchema, signInSchema } from "@/features/signin/schema";
+import { signIn } from "@/lib/auth/actions/signIn";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Card, CardContent, Stack, Typography } from "@mui/material";
+import { startTransition } from "react";
+import { useForm } from "react-hook-form";
 
 function SignInPage() {
+  const { control, handleSubmit, formState } = useForm<SignInFormSchema>({
+    resolver: zodResolver(signInSchema),
+    mode: "onSubmit",
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: SignInFormSchema) => {
+    console.log(values);
+    startTransition(async () => {
+      const result = await signIn(values);
+      if (!result.isSuccess) {
+        console.error(result.error.message);
+      }
+    });
+  };
+
   return (
     <Card variant="outlined">
       <CardContent>
         <Stack spacing={2}>
           <Typography variant="h5">Sign In</Typography>
-          <SignInEmailTextField />
-          <SignInPasswordTextField />
-          <SignInSubmitButton />
+          <SignInEmailTextField control={control} error={formState.errors} />
+          <SignInPasswordTextField control={control} error={formState.errors} />
+          <SignInSubmitButton onClick={handleSubmit(onSubmit)} />
         </Stack>
       </CardContent>
     </Card>
